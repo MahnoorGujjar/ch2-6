64688f7d51f19e361f167d926ce73ff863e0f9fb
Mahnoor Gujjar <160156566+MahnoorGujjar@users.noreply.github.com>
2024-11-06 17:18:47 +0500

    Create chapter1.js

---
+++ chp/chapter1.js
@@ -0,0 +1,22 @@
+
+/////ASSIGNMENT 01//
+
+// let username;
+// let myname="mahnoor";
+// let age= "25 years old";
+// let course="web and app development";
+// let message="Hello world";
+// let p= "pizza \n pizz\n piz\n pi\n p";
+// let email= "my email address is mahanooray1@gmail.com";
+// let book="A SMARTER WAY TO LEARN JAVASCRIPT";
+// let content="yah! I can write content through javascript";
+// alert(message);
+// alert(myname);
+// alert(age);
+// alert(course);
+// alert(p);
+// alert(email);
+// alert(book);
+// alert(content);
+
+// document.write(content);
